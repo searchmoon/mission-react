5c772ba60409061bd04ab8225c4b6f87437f8700
Rumi <moonding901212@gmail.com>
2023-12-23 23:59:32 +0900

    tanstack-query 요약 정리

--- tanstack-query/src/components/Events/NewEventsSection.jsx
+++ tanstack-query/src/components/Events/NewEventsSection.jsx
@@ -8,7 +8,7 @@ import { fetchEvents } from "../../util/http.js";
 export default function NewEventsSection() {
   const { data, isPending, error, isError } = useQuery({
     queryKey: ["events"], // query key를 이용하여 캐시 처리 하고, 나중에 동일한 요청을 전송하면 이전 요청의 응답을 재사용할 수있다.
-    queryFn: fetchEvents,
+    queryFn: fetchEvents, // promise 를 반환하는 함수를 넣어줌
     staleTime: 5000, // 불필요한 요청을 방지할 수 있다. 업데이트된 데이터를 가져오기 위한 요청을 자체적으로 전송하기 전에 기다릴 시간을 설정하는 것.
     //gcTime: 1000 //garbage collection time. 데이터와 캐시를 얼마나 오래 보관할지
   });
